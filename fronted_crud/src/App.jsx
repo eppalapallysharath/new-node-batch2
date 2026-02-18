cc6fb56965b442a1589f1c2b18f1548f1b60d2b1
eppalapallysharath <eppalapallysharath@gmail.com>
2026-02-18 10:57:13 +0530

    fronted

--- fronted_crud/src/App.jsx
+++ fronted_crud/src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   const getProducts = ()=>{
     // const url = "http://localhost:3000/products"
-    const url = "http://localhost:3000/products/2ee2a6aa-9675-4d3c-9f8e-4a410e60b3a5"
-    // const url = "https://crud-express-uawj.onrender.com"
-    fetch(url,{method:"PUT"})
+    // const url = "http://localhost:3000/products/2ee2a6aa-9675-4d3c-9f8e-4a410e60b3a5"
+    const url = "https://crud-express-uawj.onrender.com/products"
+    fetch(url, {method:"GET"})
     .then(res=>res.json())
     .then(data =>console.log(data))
     .catch(err=>console.log(err))
